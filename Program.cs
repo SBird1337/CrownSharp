1186810e14cae4c6596302261a674664435a1406
sbird <sbird@no.tld>
2022-01-15 18:53:59 +0100

    [init] csharp skeleton

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CrownSharp
+{
+    class Program
+    {
+        static void EventHandlerDolphinClientRequest(object sender, DolphinClientRequestEventArgs e)
+        {
+            DolphinServer server = sender as DolphinServer;
+            server.RecvRequest(e.Watch, e.ClockStream, e.DataStream, 0x42);
+            Logger.Instance.Info($"{e.Metadata}");
+        }
+        static void Main(string[] args)
+        {
+            DolphinServer server = new DolphinServer();
+            server.DolphinClientRequest += EventHandlerDolphinClientRequest;
+            server.StartAccept();
+        }
+    }
+}
