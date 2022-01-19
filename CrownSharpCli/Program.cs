24f941b270a924ec374bf48032ecf5d1df060f73
sbird <sbird@no.tld>
2022-01-19 01:20:41 +0100

    basic communication commands

--- CrownSharpCli/Program.cs
+++ CrownSharpCli/Program.cs
@@ -1,16 +1,36 @@
 ﻿using System;
+using System.Text;
 using GcnSharp.Core;
 using GcnSharp.Logging;
+using GcnSharp.Util;
 
 namespace CrownSharpCli
 {
     class Program
     {
+        private enum CrownLinkCommands : byte
+        {
+            PING = 10,
+            REGISTER = 11,
+            LOGIN = 12
+        };
+
         static void EventHandlerDolphinClientRequest(object sender, DolphinClientRequestEventArgs e)
         {
             DolphinServer server = sender as DolphinServer;
-            server.RecvRequest(e.Watch, e.ClockStream, e.DataStream, 0x42);
-            Logger.Instance.Info($"{e.Metadata}");
+            byte[] metaBytes = BitOperation.GetEncodedBytes(e.Metadata);
+            CrownLinkCommands command = (CrownLinkCommands)metaBytes[0];
+            switch(command)
+            {
+                case CrownLinkCommands.PING:
+                    server.RecvRequest(e.Watch, e.ClockStream, e.DataStream, BitOperation.GetDecodedUInt(Encoding.ASCII.GetBytes("ACK")));
+                    break;
+                case CrownLinkCommands.REGISTER:
+                    byte[] buffer = server.ReceiveBuffer(e.Watch, e.ClockStream, e.DataStream, 8);
+                    Logger.Instance.Info(string.Join(",", buffer));
+                    break;
+
+            }
         }
         static void Main(string[] args)
         {
